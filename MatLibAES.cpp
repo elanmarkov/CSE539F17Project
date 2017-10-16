e4feba87be00e777aaf488e0e91fcdb9015c5a58
elanmarkov <elanmarkov@gmail.com>
2017-10-16 13:31:57 -0700

    Added explanation of MatLibAES functions in comments.

--- MatLibAES.cpp
+++ MatLibAES.cpp
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include "MatLibAES.h"
 ByteArray::ByteArray(int row1, int col1) {
+// Constructor for 2D array of Bytes
+// 2D pointer; built in parts.
     row = row1;
     col = col1;
     // Create a 2D array of bytes
@@ -10,50 +12,66 @@ ByteArray::ByteArray(int row1, int col1) {
     }
 }
 ByteArray::ByteArray() {
+// Default constructor, just makes 4x4 array
     int def = 4; //default value
     ByteArray(def, def);
 }
 ByteArray::~ByteArray() {
-    // 2D array cleanup
+// Destructor, 2D array cleanup
     for(int i = 0; i < row; ++i) {
         delete [] byteArray[i];
     }
     delete [] byteArray;
 }
 Byte::Byte() {
+// Default constructor
     byte = 0;
 }
 Byte::Byte(uint8_t byte1) {
+// Assign specific byte value to byte
     byte = byte1;
 }
-uint8_t Byte::getByte() {
-    return byte;
-}
 Byte Byte::operator+(Byte rhs) {
+// Addition in this field is just XOR
     uint8_t retVal = byte ^ rhs.byte;
     return Byte(retVal);
 }
 Byte Byte::operator*(Byte rhs) {
-    uint8_t retVal;
+// AES multiplication implementation
+// This represents polynomial arithmetic in a G(256) field
+// Implemented based on NIST AES specification
+    uint8_t retVal; 
+    // working values will all be 16-bit to avoid lost data to overflow
     uint16_t workingRHS = rhs.byte;
     uint16_t workingLHS = byte;
     uint16_t mod = 0x011b; // the modulus value for polynomial arithmetic in G(256) using AES
-    uint16_t temp = 0;
+    uint16_t temp = 0; // storage of current value
     for(int i = 0; i < 8; i++) {
+    // Addition step of multiplication
+    // Add (XOR) a bit-shifted value of the LHS to the current product
+    // bit-shifted by the binary digit value (7 highest to 0 lowest) of the RHS
+    // workingLHS and workingRHS modified throughout this loop to simplify logic, reset at the end 
         workingRHS = workingRHS >> i;
-        if(workingRHS & 0x01 == 1) {
+        if(workingRHS & 0x01 == 1) { 
             workingLHS = workingLHS << i;
             temp ^= workingLHS;
-
         }
-        workingLHS = byte; // reset values after bit shift
+        // reset values after bit shift
+        workingLHS = byte; 
         workingRHS = rhs.byte;
     }
     for(int j = 0; j <8 ; j++) {
+    // Modulus loop
+    // Add (XOR) the bit-shifted value of the modulus to the current temp value
+    // The modulus will be shifted by the amount over the highest digit value (8) that
+    // represents the current degree of temp 
+    // e.g. if deg(temp) = 13 then bitshift = 13 - 8 and temp = temp ^ mod << bitshift is new value
+    // bitshift can be calculated as j - 7 based on the definition of j within the for loop.
         if(((temp << j) & 0x8000) == 0x8000) {
             temp = temp ^ (mod << 7 - j);
         }
     }
+    // Convert to 8-bit data type and return a Byte-wrapped value of it.
     retVal = temp;
     return Byte(retVal);
 }
--- MatLibAES.h
+++ MatLibAES.h
@@ -1,7 +1,14 @@
+// AES Math Library by Elan Markov
+// CSE 539 Final Project
 #include <stdint.h>
+
+// Forward declarations
 struct ByteArray;
 struct Byte;
+
 struct ByteArray {
+// Structure for creating an array of Byte items
+// only dimensions are private
     private:	
         int row, col;
     public:
@@ -10,11 +17,14 @@ struct ByteArray {
         ByteArray();
         ~ByteArray();
 };
+
+// Structure for byte items
+// Implemented as unsigned 8-bit integers
+// Perform AES specified addition and multiplication on a G(256) field
 struct Byte {
         uint8_t byte;
         Byte();
         Byte(uint8_t byte1);
-        uint8_t getByte();
         Byte operator+(Byte rhs);
         Byte operator* (Byte rhs);
 };
